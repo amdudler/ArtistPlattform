06645202c585eccefabae8c2c3145d35e60a1dbe
Marco <marco.bischof@student.htldornbirn.at>
2026-03-27 09:58:02 +0100

    Json structure done

--- backend/spotify-auth/server.js
+++ backend/spotify-auth/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+require('dotenv').config();
 
 const PORT = Number(process.env.PORT || 5050);
 const FRONTEND_URL = (process.env.FRONTEND_URL || "http://localhost:3000").replace(/\/$/, "");
